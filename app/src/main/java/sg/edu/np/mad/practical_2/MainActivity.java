d106916c2c27aa4e1f956028a5548fa5d0180ffa
pineapplez23 <beyonddl17@gmail.com>
2022-08-22 19:16:49 +0800

    Toast added but not working

--- app/src/main/java/sg/edu/np/mad/practical_2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/practical_2/MainActivity.java
@@ -4,11 +4,13 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                Log.v(TAG, "Message button pressed");
+                Toast.makeText(MainActivity.this,"Message", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -44,13 +46,16 @@ public class MainActivity extends AppCompatActivity {
                 if (MyUser.isFollowed() == false)
                 {
                     MyFollowButton.setText("Follow");
+                    Toast.makeText(MainActivity.this,"Followed", Toast.LENGTH_LONG).show();
                     MyUser.setFollowed(true);
                 }
                 else
                 {
                     MyFollowButton.setText("Unfollow");
+                    Toast.makeText(MainActivity.this, "Unfollowed", Toast.LENGTH_LONG).show();
                     MyUser.setFollowed(false);
                 }
+
             }
         });
 
